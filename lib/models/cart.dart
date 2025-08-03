fb20aeb93c8ee73fb4ae7208c0163ae751863a4f
Aniket-Vyass <aniketvyas05@gmail.com>
2025-08-03 19:25:22 +0530

    Add to Cart function added

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,6 +1,12 @@
 import 'package:flutter_catalouge/models/catalog.dart';
 
 class CartModel {
+  static final cartModel = CartModel._internal();
+
+  CartModel._internal(); //github pe singelton class me convert kia h vo likhna day 24 p fir upload krna abhi kia ni h
+
+  factory CartModel() => cartModel;
+
   //Catalog Field
   late CatalogModel _catalog;
 
@@ -17,7 +23,6 @@ class CartModel {
   //Get Items in the cart
   List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
 
-
   //Get total price
   num get totalPrice =>
       items.fold(0, (total, current) => total + current.price);
@@ -28,7 +33,7 @@ class CartModel {
     _itemIds.add(item.id);
   }
 
-//Remove Item
+  //Remove Item
   void remove(Item item) {
     _itemIds.remove(item.id);
   }
--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -1,9 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, unused_import
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'package:flutter_catalouge/models/cart.dart';
 import 'package:flutter_catalouge/models/catalog.dart';
 import 'package:flutter_catalouge/pages/home_detail_page.dart';
 import 'package:flutter_catalouge/widgets/home_widgets/catalog_image.dart';
 import 'package:flutter_catalouge/widgets/themes.dart';
-import 'package:velocity_x/velocity_x.dart';
 
 class CatalogList extends StatelessWidget {
   const CatalogList({super.key});
@@ -57,20 +61,7 @@ class CatalogItem extends StatelessWidget {
                   buttonPadding: EdgeInsets.zero,
                   children: [
                     "\$${catalog.price}".text.bold.xl.make(),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                          context
-                              .theme
-                              .floatingActionButtonTheme
-                              .backgroundColor,
-                        ),
-
-                        shape: MaterialStateProperty.all(StadiumBorder()),
-                      ),
-                      child: "Add to cart".text.white.make(),
-                    ).wh(120, 35),
+                    _AddToCart(catalog: catalog).wh(120, 35),
                   ],
                 ).pOnly(right: 8.0),
               ],
@@ -79,5 +70,38 @@ class CatalogItem extends StatelessWidget {
         ],
       ),
     ).color(context.cardColor).roundedLg.square(150).make().py16();
+  }
+}
+
+class _AddToCart extends StatefulWidget {
+  final Item catalog;
+  const _AddToCart({Key? key, required this.catalog}) : super(key: key);
+
+  @override
+  State<_AddToCart> createState() => _AddToCartState();
+}
+
+class _AddToCartState extends State<_AddToCart> {
+  bool isAdded = false;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        isAdded = isAdded.toggle();
+        final _catalog = CatalogModel();
+        final _cart = CartModel();
+        _cart.catalog = _catalog;
+        _cart.add(widget.catalog);
+        setState(() {});
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(
+          context.theme.floatingActionButtonTheme.backgroundColor,
+        ),
+
+        shape: MaterialStateProperty.all(StadiumBorder()),
+      ),
+      child: isAdded ? Icon(Icons.done) : "Add to cart".text.white.make(),
+    );
   }
 }
