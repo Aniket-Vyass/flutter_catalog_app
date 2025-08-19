86ed1e15dda2a97b2d1e74fdd2ae1bc2e9180b89
Aniket-Vyass <aniketvyas05@gmail.com>
2025-08-19 20:22:58 +0530

    Day 30: rechanged to get data locally(offline)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,31 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first, unnecessary_import
-// ignore_for_file, sort_child_properties_last
+// ignore_for_file: unused_import, sort_child_properties_last
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_catalouge/core/store.dart';
-import 'package:flutter_catalouge/models/cart.dart';
+import 'package:flutter_catalouge/pages/cart_page.dart';
 import 'package:flutter_catalouge/widgets/home_widgets/catalog_header.dart';
 import 'package:flutter_catalouge/widgets/home_widgets/catalog_list.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter_catalouge/models/catalog.dart';
 import 'package:flutter_catalouge/utils/routes.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_catalouge/widgets/drawer.dart';
+import 'package:flutter_catalouge/widgets/item_widget.dart';
+import 'package:flutter_catalouge/widgets/themes.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final url = "https://api.npoint.io/b0d70599e171c2b55341";
+  final int days = 30;
+
+  final String name = "Codepur";
 
   @override
   void initState() {
@@ -28,41 +35,25 @@ class _HomePageState extends State<HomePage> {
 
   loadData() async {
     await Future.delayed(Duration(seconds: 2));
-    // final catalogJson =
-    //     await rootBundle.loadString("assets/files/catalog.json");
-    final response = await http.get(Uri.parse(url));
-    final catalogJson = response.body;
+    final catalogJson = await rootBundle.loadString(
+      "assets/files/catalog.json",
+    );
     final decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
     CatalogModel.items =
-        List.from(
-          productsData,
-        ).map<Item>((item) => Item.fromMap(item)).toList();
+        productsData.map<Item>((item) => Item.fromMap(item)).toList();
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
       backgroundColor: context.canvasColor,
-      floatingActionButton: VxBuilder(
-        mutations: {AddMutation, RemoveMutation},
-        builder:
-            (context, store, status) => FloatingActionButton(
-              onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
-              backgroundColor:
-                  context.theme.floatingActionButtonTheme.backgroundColor,
-              child: Icon(CupertinoIcons.cart_fill, color: Colors.white),
-            ).badge(
-              color: Vx.red500,
-              size: 22,
-              count: _cart.items.length,
-              textStyle: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+        backgroundColor:
+            context.theme.floatingActionButtonTheme.backgroundColor,
+        child: Icon(CupertinoIcons.cart_fill, color: Colors.white),
       ),
       body: SafeArea(
         child: Container(
